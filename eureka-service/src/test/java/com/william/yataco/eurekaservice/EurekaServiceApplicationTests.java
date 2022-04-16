3fc5a740703d0c3ef75ce9112dfe177902c55f39
William Yataco <will.kidefw@gmail.com>
2022-04-15 22:13:11 -0500

    add eureka server

---
+++ eureka-service/src/test/java/com/william/yataco/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.yataco.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
